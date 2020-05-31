d05a9f2e78efea8eb11523c9bd889b598ad22c84
amurices <decomuricy@gmail.com>
2020-05-31 16:19:35 -0300

    AEHOOO (vou squashar)

--- src/parser.rs
+++ src/parser.rs
@@ -1,36 +1,18 @@
-use nom::character::{is_space, is_digit};
+use nom::{
+    branch::alt,
+    bytes::complete::{take_while, take_while1},
+    character::{is_space, is_digit},
+    character::complete::{char, one_of},
+    combinator::{cut, map, opt, value},
+    Err, IResult,
+};
 
 #[derive(Debug)]
-struct Expression {
-    operator: char,
-    operand1: i32,
-    operand2: i32
-}
-
 enum Expression2 {
     Num(i32),
     Expr(char, Box<Expression2>, Box<Expression2>)
 }
 
-/* Defining the polish notation parser
- * Eg:
- *      + 1 2 = 1 + 2
- */
-named!(
-    operators<Expression>,
-    do_parse!(
-        operator: one_of!("+-/*") >>
-        take_while1!(is_space) >>
-        operand1: take_while1!(is_digit) >>
-        take_while1!(is_space) >>
-        operand2: take_while1!(is_digit) >>
-        (Expression{
-            operator: operator,
-            operand1: from_u8_array_to_i32(operand1),
-            operand2: from_u8_array_to_i32(operand2)
-        })
-    )
-);
 /* WIP: Structure of recursive parser seems correct, we're fighting the nom DSL syntax now.
    An expression is either a simple literal (first branch of the alt! below) , or an open-parentheses
    expression with an operator in front. It actually doesn't have to be an operator, could be yet another
@@ -40,13 +22,61 @@ named!(
       (((((function-that-returns-another-function arg1 arg2) arg3) arg4) arg5) arg6)
 
    But we'll pretend the first element has to be an operator for now, and that the operands can be recursive. */
-named!(
-    parse_expression_top_level<Expression2>,
-    alt! (
-        parse_num |
-        parse_expression
-    )
-);
+
+fn parse_num2(i: &[u8]) -> IResult<&[u8], Expression2, (&[u8], nom::error::ErrorKind)> {
+    let first_spaces: IResult<&[u8], &[u8], (&[u8], nom::error::ErrorKind)> = take_while(is_space)(i);
+    match first_spaces {
+        Ok((rest, _)) => {
+            let digits: IResult<&[u8], &[u8], (&[u8], nom::error::ErrorKind)> = take_while1(is_digit)(rest);
+            match digits {
+                Ok((rest, num)) => {
+                    let end_spaces:  IResult<&[u8], &[u8], (&[u8], nom::error::ErrorKind)> = take_while(is_space)(rest);
+                    match end_spaces {
+                        Ok((rest, _)) => {
+                            Ok((rest, Expression2::Num(from_u8_array_to_i32(num))))
+                        }
+                        Err(_) => {
+                            panic!("Impossivel kk")
+                        }
+                    }
+                }
+                Err(_) =>
+                    panic!("Preguiça de fazer essa porra desse pattern matching funcionar")
+            }
+        }
+        Err(_) => panic!("Impossivel he!")
+    }
+}
+
+fn parse_expression2(i: &[u8]) -> IResult<&[u8], Expression2, (&[u8], nom::error::ErrorKind)> {
+    let first_spaces: IResult<&[u8], &[u8], (&[u8], nom::error::ErrorKind)> = take_while(is_space)(i);
+    // ai aqui falta a porra do operador kk mas porra da pra ver que ta indo bem a porra
+    match first_spaces {
+        Ok((rest, _)) => {
+            let operand1: IResult<&[u8], Expression2, (&[u8], nom::error::ErrorKind)> = parse_expression_top_level(rest);
+            match operand1 {
+                Ok((rest, exprOp1)) => {
+                    let operand2: IResult<&[u8], Expression2, (&[u8], nom::error::ErrorKind)> = parse_expression_top_level(rest);
+                    match operand2 {
+                        Ok((rest, _)) => {
+                            Ok((rest, Expression2::Num(from_u8_array_to_i32(num))))
+                        }
+                        Err(_) => {
+                            panic!("Impossivel kk")
+                        }
+                    }
+                }
+                Err(_) =>
+                    panic!("Preguiça de fazer essa porra desse pattern matching funcionar")
+            }
+        }
+        Err(_) => panic!("Impossivel he!")
+    }
+}
+
+fn parse_expression_top_level(i: &[u8]) -> IResult<&[u8], Expression2, (&[u8], nom::error::ErrorKind)> {
+    alt ((parse_expression, parse_num2))(i)
+}
 
 named!(
     parse_num<Expression2>,
@@ -54,7 +84,7 @@ named!(
         take_while!(is_space) >>
         num: take_while1!(is_digit) >>
         take_while!(is_space) >>
-        (Num(
+        (Expression2::Num(
             from_u8_array_to_i32(num)
         ))
     )
@@ -88,7 +118,7 @@ fn from_u8_array_to_i32(input: &[u8]) -> i32{
 }
 
 pub fn parse(input : &str) -> String {
-    let expression = operators(input.as_bytes());
+    let expression = parse_expression_top_level(input.as_bytes());
     println!("{:?}", expression);
     // FIXME: no explaination needed here unless you have Deco's IQ
     String::from("aaa")
