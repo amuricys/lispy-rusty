8af1e2b5a73f5b71af4052e5d8af13506240e928
Deco <decomuricy@gmail.com>
2020-06-06 14:17:45 -0300

    Use fns instead of relying on named! macro (#1)
    This will help make our life easier with recursion and such. Most of the struggle is figuring out the return types of the different constructs `nom` gives us. My understanding is that, when you have a macro named `my_macro`, say, and it takes a single argument and returns a function, doing `my_macro!(some_arg)` both _expands_ the macro _and_ calls the resulting function it produces. However, doing `my_macro(some_arg)` (without the `bang!`) simply returns the function it produces, meaning something like this `my_macro(some_arg)(some_other_arg)` could make sense (and that's how this PR is using `take_while` among others).
    
    EDIT: This style is very reminiscent of `do` notation in Haskell. The `?` operator suggested by the God @gabaconrado gives us a short circuiting way of piping the the result of parsers to the next ones.
    
    The next step is to implement variadic arguments and expressions as operators (the first argument to a list doesn't have to be a `char`, it can be an entire expression). Also, not every atom is a Num, so we'll make that into a rich enum as well.

--- src/parser.rs
+++ src/parser.rs
@@ -1,38 +1,21 @@
-use nom::character::{is_space, is_digit};
+use nom::{
+    branch::alt,
+    bytes::complete::{take_while, take_while1},
+    sequence::delimited,
+    character::{is_space, is_digit},
+    character::complete::{char, one_of},
+    combinator::{cut, map, opt, value},
+    Err, IResult,
+};
 
 #[derive(Debug)]
-struct Expression {
-    operator: char,
-    operand1: i32,
-    operand2: i32
-}
-
-enum Expression2 {
+pub enum Expression {
     Num(i32),
-    Expr(char, Box<Expression2>, Box<Expression2>)
+    Expr(char, Box<Expression>, Box<Expression>),
 }
 
-/* Defining the polish notation parser
- * Eg:
- *      + 1 2 = 1 + 2
- */
-named!(
-    operators<Expression>,
-    do_parse!(
-        operator: one_of!("+-/*") >>
-        take_while1!(is_space) >>
-        operand1: take_while1!(is_digit) >>
-        take_while1!(is_space) >>
-        operand2: take_while1!(is_digit) >>
-        (Expression{
-            operator: operator,
-            operand1: from_u8_array_to_i32(operand1),
-            operand2: from_u8_array_to_i32(operand2)
-        })
-    )
-);
 /* WIP: Structure of recursive parser seems correct, we're fighting the nom DSL syntax now.
-   An expression is either a simple literal (first branch of the alt! below) , or an open-parentheses
+   An expression is either a simple literal (first branch of the alt below) , or an open-parentheses
    expression with an operator in front. It actually doesn't have to be an operator, could be yet another
    open-parentheses expression. Lisp expressions can simply be lists of n other expressions,
    meaning the following is valid:
@@ -40,56 +23,43 @@ named!(
       (((((function-that-returns-another-function arg1 arg2) arg3) arg4) arg5) arg6)
 
    But we'll pretend the first element has to be an operator for now, and that the operands can be recursive. */
-named!(
-    parse_expression_top_level<Expression2>,
-    alt! (
-        parse_num |
-        parse_expression
-    )
-);
 
-named!(
-    parse_num<Expression2>,
-    do_parse!(
-        take_while!(is_space) >>
-        num: take_while1!(is_digit) >>
-        take_while!(is_space) >>
-        (Num(
-            from_u8_array_to_i32(num)
-        ))
-    )
-);
-
-named!(
-    parse_expression<Expression2>,
-    delimited!(
-        char!('('),
-        do_parse!(
-            operator: one_of!("+-/*") >>
-            take_while1!(is_space) >>
-            operand1: parse_expression_top_level() >>
-            take_while1!(is_space) >>
-            operand2: parse_expression_top_level() >>
-            take_while1!(is_space) >>
-            (Expr(
-                operator,
-                operand1,
-                operand2
-             ))),
-        char!(')'))
-
-);
-
-fn from_u8_array_to_i32(input: &[u8]) -> i32{
-    std::str::from_utf8(input)
-            . expect("Error byte array -> string")
-            . parse()
-            . expect("Error string -> i32")
+fn parse_expression_top_level(i: &[u8]) -> IResult<&[u8], Expression, (&[u8], nom::error::ErrorKind)> {
+    alt((parse_num, parse_expression))(i)
 }
 
-pub fn parse(input : &str) -> String {
-    let expression = operators(input.as_bytes());
-    println!("{:?}", expression);
-    // FIXME: no explaination needed here unless you have Deco's IQ
-    String::from("aaa")
+fn parse_num(i: &[u8]) -> IResult<&[u8], Expression, (&[u8], nom::error::ErrorKind)> {
+    let (rest, _first_spaces) = take_while(is_space)(i)?;
+    let (rest, digit) = take_while1(is_digit)(rest)?;
+    let (rest, _end_spaces) = take_while(is_space)(rest)?;
+
+    Ok((rest, Expression::Num(from_u8_array_to_i32(digit))))
+}
+
+fn parse_expression(i: &[u8]) -> IResult<&[u8], Expression, (&[u8], nom::error::ErrorKind)> {
+    let (rest, _paren1) = char('(')(i)?;
+    let (rest, _first_spaces) = take_while(is_space)(rest)?;
+    let (rest, op) = one_of("+-/*")(rest)?;
+    let (rest, _spaces_between_operator_and_first_operand) = take_while1(is_space)(rest)?;
+    let (rest, expr_op1) = parse_expression_top_level(rest)?;
+    let (rest, expr_op2) = parse_expression_top_level(rest)?;
+    let (rest, _paren2) = char(')')(rest)?;
+
+    Ok((rest, Expression::Expr(op, Box::new(expr_op1), Box::new(expr_op2))))
+}
+
+fn from_u8_array_to_i32(input: &[u8]) -> i32 {
+    std::str::from_utf8(input)
+        .expect("Error byte array -> string")
+        .parse()
+        .expect("Error string -> i32")
+}
+
+pub fn parse(input: &str) -> IResult<&[u8], Expression, (&[u8], nom::error::ErrorKind)> {
+    let expression = parse_expression_top_level(input.as_bytes());
+    expression
+}
+
+pub fn eval (expr: Expression) -> String {
+     format!("{:?}", expr)
 }
